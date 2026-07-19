82ea5f037111b46ae42cd37af74e8af8437739cb
Cloud User <ec2-user@ip-172-31-24-125.ec2.internal>
2026-07-19 09:29:49 +0000

    Updated and fixed deployment files

--- App/index.js
+++ App/index.js
@@ -1,8 +1,4 @@
 const express = require('express')
-const app = express()
-
-app.get('/', (req,res) => {
-	res.send(const express = require('express')
 const mysql = require('mysql2')
 const app = express()
 
@@ -11,8 +7,8 @@ app.use(express.urlencoded({ extended: true }))
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'db',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'devops'
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'names'
 })
 
 db.connect()
